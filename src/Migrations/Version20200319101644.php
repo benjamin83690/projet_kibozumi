cc9d8fd5ef996eb6641bc74d0a05e9ea37649d96
clement-lopez84 <61272100+clement-lopez84@users.noreply.github.com>
2020-03-19 13:35:10 +0100

    modif

--- src/Migrations/Version20200319101644.php
+++ src/Migrations/Version20200319101644.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200319081909 extends AbstractMigration
+final class Version20200319101644 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200319081909 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE credit (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, mensualites INT NOT NULL, nombres_mensualites INT NOT NULL, montant_emprunte INT NOT NULL, taux_fixe DOUBLE PRECISION NOT NULL, montant_total DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE credit (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, mensualites INT NOT NULL, nombres_mensualites INT NOT NULL, montant_emprunte INT NOT NULL, taux_fixe DOUBLE PRECISION NOT NULL, montant_total DOUBLE PRECISION NOT NULL, image_name VARCHAR(255) DEFAULT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, telephone INT NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_credit (user_id INT NOT NULL, credit_id INT NOT NULL, INDEX IDX_799375C3A76ED395 (user_id), INDEX IDX_799375C3CE062FF9 (credit_id), PRIMARY KEY(user_id, credit_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_credit ADD CONSTRAINT FK_799375C3A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
