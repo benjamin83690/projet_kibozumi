7dd68d353c97fb1a9d862eaf6c19389c533dc4e2
Benjamin Fichot <fichot.benjamin@gmail.com>
2020-03-20 13:58:35 +0100

    Update Credit.php

--- src/Entity/Credit.php
+++ src/Entity/Credit.php
@@ -170,83 +170,82 @@ class Credit
     }
 
     /**
-        * NOTE: This is not a mapped field of entity metadata, just a simple property.
-        * 
-        * @Vich\UploadableField(mapping="credits_image", fileNameProperty="imageName")
-        * @Assert\Image(
-        * mimeTypesMessage = "choisir une image"
-        
-        * )
-        * 
-        * @var File|null
-        */
-        private $imageFile;
+    * NOTE: This is not a mapped field of entity metadata, just a simple property.
+    * 
+    * @Vich\UploadableField(mapping="credits_image", fileNameProperty="imageName")
+    * @Assert\Image(
+    * mimeTypesMessage = "choisir une image"
+    
+    * )
+    * 
+    * @var File|null
+    */
+    private $imageFile;
 
-        /**
-        * @ORM\Column(type="string", nullable = true)
-        *
-        * @var string|null
-        */
-        private $imageName;
+    /**
+    * @ORM\Column(type="string", nullable = true)
+    *
+    * @var string|null
+    */
+    private $imageName;
 
 
-        /**
-        * @ORM\Column(type="datetime")
-        *
-        * @var \DateTimeInterface|null
-        */
-        private $updatedAt;
+    /**
+    * @ORM\Column(type="datetime")
+    *
+    * @var \DateTimeInterface|null
+    */
+    private $updatedAt;
 
-        /**
-        * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-        * of 'UploadedFile' is injected into this setter to trigger the update. If this
-        * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-        * must be able to accept an instance of 'File' as the bundle will inject one here
-        * during Doctrine hydration.
-        *
-        * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
-        */
-        public function setImageFile(?File $imageFile = null): void
-        {
-            $this->imageFile = $imageFile;
+    /**
+    * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+    * of 'UploadedFile' is injected into this setter to trigger the update. If this
+    * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+    * must be able to accept an instance of 'File' as the bundle will inject one here
+    * during Doctrine hydration.
+    *
+    * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+    */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
 
-            if (null !== $imageFile) {
-                // It is required that at least one field changes if you are using doctrine
-                // otherwise the event listeners won't be called and the file is lost
-                $this->updatedAt = new \DateTimeImmutable();
-            }
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
         }
+    }
 
-        public function getImageFile(): ?File
-        {
-            return $this->imageFile;
-        }
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
 
-        public function setImageName(?string $imageName): void
-        {
-            $this->imageName = $imageName;
-        }
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
 
-        public function getImageName(): ?string
-        {
-            return $this->imageName;
-        }
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
 
-                /////////////////////////////////
+    /////////////////////////////////
 
-                public function getNewMensualites($montant)
-                {
-                    $taux= 0.0295;
-                    $nombresMensualites=30;
-                    $a=$montant * $taux;
-                    return $this->mensualites=($a + $montant)/$nombresMensualites;
-                
-                }
-                public function getNewMontantTotal($montant)
-                {
-                    $taux= 0.0295;
-                    $a=$montant * $taux;
-                    return $this->montantTotal=($a + $montant);
-                
-                }
+    public function getNewMensualites($montant)
+    {
+        $taux= 0.0295;
+        $nombresMensualites=30;
+        $a=$montant * $taux;
+        return $this->mensualites=($a + $montant)/$nombresMensualites;
+    }
+    
+    public function getNewMontantTotal($montant)
+    {
+        $taux= 0.0295;
+        $a=$montant * $taux;
+        return $this->montantTotal=($a + $montant);
+    }
 }
