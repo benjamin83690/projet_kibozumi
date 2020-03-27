117cd1f90d792d6340cd92bf9ad02eb043095f49
Benjamin Fichot <fichot.benjamin@gmail.com>
2020-03-27 13:25:43 +0100

    debut du routing

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Entity\Credit;
 use App\Entity\Category;
 use App\Entity\Commande;
+use App\Form\CreditType;
 use App\Form\CategoryType;
 use App\Repository\CreditRepository;
 use App\Repository\CategoryRepository;
@@ -15,13 +16,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
+
 /**
- * @Route("/category")
+ * @Route("/admin")
  */
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/", name="category_index", methods={"GET"})
+     * @Route("/category", name="category_index", methods={"GET"})
      */
     public function index(CategoryRepository $categoryRepository): Response
     {
@@ -31,7 +33,7 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="category_new", methods={"GET","POST"})
+     * @Route("/create", name="category_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
@@ -39,9 +41,14 @@ class CategoryController extends AbstractController
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
 
+        $credit = new Credit();
+        $form2 = $this->createForm(CreditType::class, $credit);
+        $form2->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($category);
+            $entityManager->persist($credit);
             $entityManager->flush();
 
             return $this->redirectToRoute('category_index');
@@ -49,23 +56,14 @@ class CategoryController extends AbstractController
 
         return $this->render('category/new.html.twig', [
             'category' => $category,
+            'credit' => $credit,
             'form' => $form->createView(),
+            'form2' => $form2->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="category_show", methods={"GET", "POST"})
-     */
-    public function show(Category $category, CreditRepository $credits): Response
-    {
-        return $this->render('category/show.html.twig', [
-            'category' => $category,
-            'credits' => $credits->findByCategory($category->getId()),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="category_edit", methods={"GET","POST"})
+     * @Route("/category/{id}/edit", name="category_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Category $category): Response
     {
@@ -85,7 +83,7 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="category_delete", methods={"DELETE"})
+     * @Route("/category/{id}", name="category_delete", methods={"DELETE"})
      */
     public function delete(Request $request, Category $category): Response
     {
@@ -96,34 +94,5 @@ class CategoryController extends AbstractController
         }
 
         return $this->redirectToRoute('category_index');
-    }
-
-    /**
-    * @Route("/ajax/{user}/{id}", name="ajax", methods={"GET"})
-    */
-    public function ajax(Request $request, User $user, Credit $credit ): Response
-    {
-        $montant = $request->query->get('montant');
-        $mensualites = $request->query->get('mensualites');
-        $montantTotal = $request->query->get('montantTotal');
-        $creditId = $request->query->get('creditId');
-        
-        $data = [
-            'montantEmprunte'=> intval($montant),
-            'mensualites'=> floatval($mensualites),
-            'montantTotal'=> floatval($montantTotal),
-            'creditId'=> intval($creditId)
-        ];
-        $commande = new Commande();
-        $entityManager = $this->getDoctrine()->getManager();
-        $commande->setMontantEmprunte($montant)
-                ->setMontantTotal($montantTotal)
-                ->setMensualites($mensualites)
-                ->setUserCommande($user)
-                ->setCreditCommande($credit)
-        ;
-        $entityManager->persist($commande);
-        $entityManager->flush();
-        return $this->json($data);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,14 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Entity\Credit;
+use App\Entity\Category;
+use App\Entity\Commande;
+use App\Repository\CreditRepository;
 use App\Repository\CategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -37,5 +44,43 @@ class HomeController extends AbstractController
         ]);
     }
 
-    
+    /**
+    * @Route("/commande/{id}", name="category_show", methods={"GET", "POST"})
+    */
+    public function show(Category $category, CreditRepository $credits): Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'credits' => $credits->findByCategory($category->getId()),
+        ]);
+    }
+
+    /**
+    * @Route("/ajax/{user}/{id}", name="ajax", methods={"GET"})
+    */
+    public function ajax(Request $request, User $user, Credit $credit ): Response
+    {
+        $montant = $request->query->get('montant');
+        $mensualites = $request->query->get('mensualites');
+        $montantTotal = $request->query->get('montantTotal');
+        $creditId = $request->query->get('creditId');
+        
+        $data = [
+            'montantEmprunte'=> intval($montant),
+            'mensualites'=> floatval($mensualites),
+            'montantTotal'=> floatval($montantTotal),
+            'creditId'=> intval($creditId)
+        ];
+        $commande = new Commande();
+        $entityManager = $this->getDoctrine()->getManager();
+        $commande->setMontantEmprunte($montant)
+                ->setMontantTotal($montantTotal)
+                ->setMensualites($mensualites)
+                ->setUserCommande($user)
+                ->setCreditCommande($credit)
+        ;
+        $entityManager->persist($commande);
+        $entityManager->flush();
+        return $this->json($data);
+    }
 }
